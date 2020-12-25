1493523139a7c16b9abc63fa17f107859b95b426
prodabdab <73546028+prodabdab@users.noreply.github.com>
2020-12-26 00:25:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var sayy={Margarita}
+var sayy1={Fammfresh}
+
+var mainmenu=[]
+
+function farrrrm () {
+        mainmenu.push(sayy);
+    }
+
+function farrrrm1 () {
+    mainmenu.push(sayy1)
+}
+function getmenu() {
+    document.getElementById("boi").innerHTML=mainmenu;
+}
